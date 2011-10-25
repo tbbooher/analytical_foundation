71027840389b3f0b42eb51e5203462ac293178c2
Tim Booher <ads@theboohers.org>
2011-10-25 08:23:34 -0400

    updated

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,2 +1,7 @@
 class ProblemsController < InheritedResources::Base
+  actions :all, :except => :show
+  def show
+    @problem = Problem.find(params[:id])
+    @solutions = Solution.where(problem_id: @problem.id)
+  end
 end
