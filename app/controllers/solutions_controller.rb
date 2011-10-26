92347d55df2372803853ef332424a383d5dbcd6d
Tim Booher <ads@theboohers.org>
2011-10-25 22:37:32 -0400

    added solutions and front-end logic

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -1,4 +1,10 @@
 class SolutionsController < InheritedResources::Base
   belongs_to :problem
   load_and_authorize_resource
+
+  def create
+    super do |format|
+      format.html { redirect_to problem_path(@problem) }
+    end
+  end
 end
